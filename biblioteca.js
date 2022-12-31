b722361b1409cac314a77b43632e64956a18bc33
Matias Belvedere <matiasbelvedere@hotmail.com>
2022-12-31 18:05:57 -0300

    convirtiendo en obj los paises de la biblioteca

--- biblioteca.js
+++ biblioteca.js
@@ -1,23 +1,85 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     fetchData();
-// });
+document.addEventListener("DOMContentLoaded", () => {
+    fetchDataAfr();
+    fetchDataAme();
+    fetchDataAsi();
+    fetchDataEur();
+    fetchDataOce();
+});
 
-// const fetchData = async () => {
-//     try {
-//         const res = await fetch("https://restcountries.com/v2/all");
-//         const data = await res.json();
-//         // innerData(data);
-//         console.log(data);
-//     } catch {
-        
-//     };
-// };
+const nombrePaisesAfr = [];
+const nombrePaisesAme = [];
+const nombrePaisesAsi = [];
+const nombrePaisesEur = [];
+const nombrePaisesOce = [];
+
+class Pais {
+    constructor(name, flag) {
+        this.name = name;
+        this.flag = flag;
+    }
+}
+
+const mostrarTodo = document.getElementById("mostrarTodo");
+mostrarTodo.addEventListener("click", () => {
+    arr.forEach(e => {
+        e.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+        e.style.color = "white";
+    });
+    mostrarTodo.style.backgroundColor = "white";
+    mostrarTodo.style.color = "black";
+    biblioteca.innerHTML = "";
+    fetchDataAfr();
+    fetchDataAme();
+    fetchDataAsi();
+    fetchDataEur();
+    fetchDataOce();
+})
 
 const fetchDataAfr = async () => {
     try {
         const res = await fetch("https://restcountries.com/v3.1/region/africa");
         const data = await res.json();
-        innerData(data);
+        //borrando paises
+        data.splice(5,2);data.splice(18,1); data.splice(23,1); data.splice
+        (25,1); data.splice(28,1); data.splice(29,1); data.splice(35,2);data.splice(40,1); data.splice(44,1);
+        //poniendo los paises en un array
+        if (nombrePaisesAfr.length === 0) {
+            data.forEach(e => {
+                nombrePaisesAfr.push(new Pais(e.translations.spa.common.toUpperCase(), e.flags.png))
+            })
+        }
+        //ordenar alfabeticamente el arr
+        nombrePaisesAfr.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1
+            }
+            if (a.name > b.name) {
+                return 1
+            }
+            return 0
+        })
+        const botsFlag = nombrePaisesAfr[3].flag;
+        const repCongFlag = nombrePaisesAfr[9].flag;
+        const malaFlag = nombrePaisesAfr[28].flag;
+        const lesoFlag = nombrePaisesAfr[35].flag;
+        const leoFlag = nombrePaisesAfr[41].flag;
+        //corrigiendo nombres
+        nombrePaisesAfr.splice(3,1,{name: "BOTSUANA", flag: botsFlag});
+        nombrePaisesAfr.splice(9,1,{name: "REPÚBLICA DEMOCRÁTICA DEL CONGO", flag: repCongFlag});
+        nombrePaisesAfr.splice(28,1,{name: "MALAUI", flag: malaFlag});
+        nombrePaisesAfr.splice(35,1,{name: "LESOTO", flag: lesoFlag});
+        nombrePaisesAfr.splice(41,1,{name: "SIERRA LEONA", flag: leoFlag});
+        //ordenar alfabeticamente el arr
+        nombrePaisesAfr.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1
+            }
+            if (a.name > b.name) {
+                return 1
+            }
+            return 0
+        })
+        innerData(data, nombrePaisesAfr);
     } catch {
         
     };
@@ -27,9 +89,27 @@ const fetchDataAme = async () => {
     try {
         const res = await fetch("https://restcountries.com/v3.1/region/america");
         const data = await res.json();
-        innerData(data);
+        //borrando paises
+        data.splice(0,1); data.splice(2,1); data.splice(3,1); data.splice(5,
+        3);data.splice(6,3);data.splice(14,7);data.splice(15,1); data.splice(19,1); data.splice(20,1); data.splice(22,2);data.splice(24,1); data.splice(26,2); data.splice(28,1); data.splice(30,1);
+        //poniendo los paises en un array
+        if (nombrePaisesAme.length === 0) {
+            data.forEach(e => {
+                nombrePaisesAme.push(new Pais(e.translations.spa.common.toUpperCase(), e.flags.png));
+            })
+        }
+        //ordenar alfabeticamente el arr
+        nombrePaisesAme.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1
+            }
+            if (a.name > b.name) {
+                return 1
+            }
+            return 0
+        })
+        innerData(data, nombrePaisesAme);
     } catch {
-        
     };
 };
 
@@ -37,7 +117,31 @@ const fetchDataAsi = async () => {
     try {
         const res = await fetch("https://restcountries.com/v3.1/region/asia");
         const data = await res.json();
-        innerData(data);
+        //borrando paises
+        data.splice(9,1); data.splice(12,1); data.splice(24,1); data.splice
+        (44,2);
+        //poniendo los paises en un array
+        if (nombrePaisesAsi.length === 0) {
+            data.forEach(e => {
+                nombrePaisesAsi.push(new Pais(e.translations.spa.common.toUpperCase(), e.flags.png));
+            })
+        }
+        //corrigiendo nombres
+        const iranFlag = nombrePaisesAsi[1].flag;
+        const barFlag = nombrePaisesAsi[38].flag;
+        nombrePaisesAsi.splice(1,1,{name: "IRÁN", flag: iranFlag});
+        nombrePaisesAsi.splice(38,1,{name: "BARÉIN", flag: barFlag})
+        //ordenar alfabeticamente el arr
+        nombrePaisesAsi.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1
+            }
+            if (a.name > b.name) {
+                return 1
+            }
+            return 0
+        })
+        innerData(data, nombrePaisesAsi);
     } catch {
         
     };
@@ -47,7 +151,35 @@ const fetchDataEur = async () => {
     try {
         const res = await fetch("https://restcountries.com/v3.1/region/europe");
         const data = await res.json();
-        innerData(data);
+        //borrando paises
+        data.splice(9,1); data.splice(12,1); data.splice(14,1); data.splice
+        (25,2); data.splice(32,1);
+        //poniendo los paises en un array
+        if (nombrePaisesEur.length === 0) {
+            data.forEach(e => {
+                nombrePaisesEur.push(new Pais(e.translations.spa.common.toUpperCase(), e.flags.png));
+            })
+        }
+        //corrigiendo nombres
+        const feroFlag = nombrePaisesEur[1].flag;
+        const esloFlag = nombrePaisesEur[2].flag;
+        const cheFlag = nombrePaisesEur[16].flag;
+        const rumaFlag = nombrePaisesEur[30].flag;
+        nombrePaisesEur.splice(1,1,{name: "ISLAS FEROE", flag: feroFlag});
+        nombrePaisesEur.splice(2,1,{name: "ESLOVAQUIA", flag: esloFlag});
+        nombrePaisesEur.splice(16,1,{name: "REPÚBLICA CHECA", flag: cheFlag});
+        nombrePaisesEur.splice(30,1,{name: "RUMANÍA", flag: rumaFlag});
+        //ordenar alfabeticamente el arr
+        nombrePaisesEur.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1
+            }
+            if (a.name > b.name) {
+                return 1
+            }
+            return 0
+        })
+        innerData(data, nombrePaisesEur);
     } catch {
         
     };
@@ -57,7 +189,25 @@ const fetchDataOce = async () => {
     try {
         const res = await fetch("https://restcountries.com/v3.1/region/oceania");
         const data = await res.json();
-        innerData(data);
+        //borrando paises
+        data.splice(1,4);data.splice(2,9);data.splice(3,11);
+        //poniendo los paises en un array
+        if (nombrePaisesOce.length === 0) {
+            data.forEach(e => {
+                nombrePaisesOce.push(new Pais(e.translations.spa.common.toUpperCase(), e.flags.png));
+            })
+        }
+        //ordenar alfabeticamente el arr
+        nombrePaisesOce.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1
+            }
+            if (a.name > b.name) {
+                return 1
+            }
+            return 0
+        })
+        innerData(data, nombrePaisesOce);
     } catch {
         
     };
@@ -70,27 +220,16 @@ const ameBtn = document.getElementById("ameBtn");
 const asiBtn = document.getElementById("asiBtn");
 const eurBtn = document.getElementById("eurBtn");
 const oceBtn = document.getElementById("oceBtn");
-const arr = [afrBtn, ameBtn, asiBtn, eurBtn, oceBtn];
+const arr = [afrBtn, ameBtn, asiBtn, eurBtn, oceBtn, mostrarTodo];
 
-
-const innerData = data => {
-    // ordenar paises alfabeticamente
-    data.sort((a, b) => {
-        if (a.translations.spa.common < b.translations.spa.common) {
-            return -1
-        }
-        if (a.translations.spa.common > b.translations.spa.common) {
-            return 1
-        }
-        return 0
-    })
+const innerData = (data, array) => {
     // agregar paises
     data.forEach((e, i) => {
         biblioteca.innerHTML += 
         `
         <div class="cardBiblio">
-            <img class="flagImage" src="${e.flags.png}" alt="flag">
-            <p class="nombrePais">${e.translations.spa.common}</p>
+            <img class="flagImage" src="${array[i].flag}" alt="flag">
+            <p class="nombrePais">${array[i].name}</p>
         </div>
         `;
     })
@@ -150,3 +289,4 @@ oceBtn.addEventListener("click", () => {
     biblioteca.innerHTML = "";
     fetchDataOce();
 });
+
--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const reset = document.getElementById("reset");
 const mostrar = document.getElementById("mostrar");
 const input = document.querySelector(".input");
 const moreInfo = document.querySelector(".moreInfo");
-const i2 = document.getElementById("i2");
 const point = document.querySelector(".point");
 let contador = sessionStorage.getItem("contador");
-point.innerHTML = `PUNTOS: ${contador}`
+point.innerHTML = `PUNTOS: <span style="color: greenyellow;">${contador}</span>`
 if (typeof contador === "object") point.innerHTML = `puntos: 0`
 
 const fetchData = async () => {
@@ -26,23 +25,32 @@ const fetchData = async () => {
     };
 };
 
-const letraSeleccionada = document.querySelector(".letraSeleccionada");
 const imageContainer = document.querySelector(".imageContainer");
-let rand = Math.random() * 250;
+let rand = Math.random() * 184;
 console.log(parseInt(rand));
 const nombrePaises = [];
 
 const adivinar = data => {
+    
+    //eliminación de paises
+    /* anguilla */data.splice(7, 1); /* bermuda */data.splice(23, 1); /* caribe neerlandes */data.splice(25, 1);/*bouvet */data.splice(27, 1);/*territorio britanico del oceano indico */data.splice(28, 1);/*islas virgenes de eeuu */data.splice(30, 1);/*cabo verde */data.splice(37, 1);/*islas caiman */data.splice(37, 1);/*isla de navidad */data.splice(41, 1);/*islas cocos */data.splice(41, 1);/*comoras */data.splice(42, 1); /*islas cook */data.splice(44, 1);/*curazao */data.splice(47, 1);/*dominica */data.splice(51, 1);/*fiji */data.splice(61, 1);/*polinesia francesa */data.splice(64, 1);/*territorios australes y antarticos franceses */data.splice(64, 1);/*grenada */data.splice(72, 1);/*guadaloupe */data.splice(72, 1);/*guam */data.splice(72, 1);/*guernsey */data.splice(73, 1);/* Islas Heard y McDonald */data.splice(77, 1);/*isla de man */data.splice(88, 1); /*jersey */data.splice(92, 1); /*kiribati*/ data.splice(95, 1); /* macao*/ data.splice(106, 1); /*maldivas */data.splice(110, 1); /*islas marshall*/data.splice(112, 1);/*martinica */ data.splice(112, 1); /*mauricio*/ data.splice(113, 1); /*mayote*/ data.splice(113, 1); /*micronesia */ data.splice(114, 1); /*monserrat */ data.splice(118, 1); /*nauru */ data.splice(122, 1);/*nueva caledonia */ data.splice(124, 1); /*niue */ data.splice(128, 1); /*isla norfolk */ data.splice(128, 1); /*islas marianas del norte */ data.splice(129, 1); /*palau */ data.splice(132, 1); /*islas pitcairn */ data.splice(138, 1); /*pais reunion */ data.splice(143, 1); /*san bartolome */ data.splice(146, 1); /*santa elena */ data.splice(146, 1); /*san cristobal y nieves */ data.splice(146, 1); /*santa lucia */ data.splice(146, 1); /*saint martin  */ data.splice(146, 1); /*san pedro y miquelon */ data.splice(146, 1); /*san vicente y granadinas */ data.splice(146, 1); /*samoa */ data.splice(146, 1); /*santo tome y principe  */ data.splice(147, 1); /*seychelles */ data.splice(150, 1); /*sint maarten */ data.splice(152, 1); /*islas georgias y sandinwch */ data.splice(157, 1); /*svalbard */ data.splice(163, 1); /*islas tokelau  */data.splice(173, 1); /*tonga */ data.splice(173, 1); /*islas turks y caicos  */ data.splice(177, 1); /*tuvalu */ data.splice(177, 1); /*vanuatu */ data.splice(184, 1); /*wallis y fortuna */ data.splice(186, 1); /*alandia */ data.splice(1, 1); /*aruba */ data.splice(10, 1); /*samoa americana */ data.splice(3, 1); /*islas virgenes r. unido */ data.splice(26, 1);
+    /*estados unidos 2 */ data.splice(25, 1); /*malvinas */ data.splice(54, 1);
+    //agregar puntos al nro de poblacion
     let poblacion = data[parseInt(rand)].population;
     let poblacionPoint = new Intl.NumberFormat("es-ES").format(poblacion)
 
+    input.focus()
     input.value = ""
     // pasar todos los nombre a un arr en mayus
     data.forEach(e => {
         let nombreMayus = e.translations.es.toUpperCase();
         nombrePaises.push(nombreMayus);
     });
-    let paisSelec = nombrePaises[parseInt(rand)]
+
+    //cambiando nombre al correcto español
+    nombrePaises.splice(13, 1, "BARÉIN"); nombrePaises.splice(23, 1, "BOTSUANA"); nombrePaises.splice(38, 1, "REPÚBLICA DEMOCRÁTICA DEL CONGO"); nombrePaises.splice(54, 1, "ISLAS FEROE"); nombrePaises.splice(70, 1, "HAITÍ"); nombrePaises.splice(71, 1, "CIUDAD DEL VATICANO"); nombrePaises.splice(79, 1, "IRÁN"); nombrePaises.splice(90, 1, "KIRGUISTÁN"); nombrePaises.splice(94, 1, "LESOTO"); nombrePaises.splice(100, 1, "MACEDONIA DEL NORTE"); nombrePaises.splice(102, 1, "MALAUI"); nombrePaises.splice(104, 1, "MALÍ"); nombrePaises.splice(137, 1, "RUMANÍA"); nombrePaises.splice(144, 1, "SIERRA LEONA"); nombrePaises.splice(146, 1, "ESLOVAQUIA"); nombrePaises.splice(150, 1, "SUDÁFRICA"); nombrePaises.splice(157, 1, "ESUATINI"); nombrePaises.splice(141, 1, "ARABIA SAUDITA");
+    //pais seleccionado
+    let paisSelec = nombrePaises[parseInt(rand)];
     console.log(paisSelec);
 
     mostrar.addEventListener("click", () => {
@@ -81,7 +89,7 @@ const adivinar = data => {
                 <p style="color: greenyellow;">Mas info: <a style="color: lightblue;" target="_blank" href="https://es.wikipedia.org/wiki/${paisSelec.toLowerCase()}">CLICK AQUÍ</a></p>
                 `
                 contador++;
-                point.innerHTML = `PUNTOS: ${contador}`;
+                point.innerHTML = `PUNTOS: <span style="color: greenyellow;">${contador}</span>`;
                 sessionStorage.setItem("contador", contador);
                 reset.focus();
                 input.readOnly = true;
